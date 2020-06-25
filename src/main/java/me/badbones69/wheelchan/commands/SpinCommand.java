3bf85cdc09ba7cb8ce4286c9aa1d1893c8698fbb
BadBones69 <joewojcik14@gmail.com>
2020-06-24 23:57:33 -0400

    Fix Random

--- src/main/java/me/badbones69/wheelchan/commands/SpinCommand.java
+++ src/main/java/me/badbones69/wheelchan/commands/SpinCommand.java
@@ -6,19 +6,19 @@ import me.badbones69.wheelchan.api.objects.Senpai;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class SpinCommand {
     
     private static WheelChan wheelChan = WheelChan.getInstance();
-    private static Random random = ThreadLocalRandom.current();
+    private static Random random = new Random();
     
     public static void runCommand(MessageReceivedEvent e) {
         Senpai senpai = getRandomSenpai();
         e.getChannel().sendMessage(Messages.SPIN_WHEEL.getMessage(e.getGuild(), "%senpai%", senpai.getUser().getAsMention()).build()).complete();
     }
     
-    private static Senpai getRandomSenpai() {
+    public static Senpai getRandomSenpai() {
+        random.setSeed(System.nanoTime());
         return wheelChan.getSenpais().get(random.nextInt(wheelChan.getSenpais().size()));
     }
     
